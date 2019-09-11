2dca5129f2f272e6a1562892262ebb1fe284d619
oleksandr.solodko <olexanders17@gmail.com>
2019-09-11 17:21:17 +0300

    uodate main.py

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print ('hello')
-print ('hello')
+from enum import Enum
+
+
+class Days(Enum):
+    MONDAY = 1
+    TUESDAY = 2
+    WEDNESDAY = 3
+    THURSDAY = 4
+    FRIDAY = 5
+    SATURDAY = 6
+    SUNDAY = 7
+
+
+class Time():
+
+    def __init__(self, min=0, hour=0):
+        self.min = min
+        self.hour = hour
+
+
+time=Time()
