a98d15d5173a32e7143dca3f1d5f684cac54e923
oleksandr.solodko <olexanders17@gmail.com>
2019-09-12 16:32:49 +0300

    1

--- main.py
+++ main.py
@@ -14,8 +14,26 @@ class Days(Enum):
 class Time():
 
     def __init__(self, min=0, hour=0):
-        self.min = min
-        self.hour = hour
+        self._min = min
+        self._hour = hour
+
+    @property
+    def min(self):
+        return self._min
+
+    @min.setter
+    def min(self, val):
+        if val >= 0 and val <= 59:
+            self._min = val
+        else:
+            self._min = 0
+
+    def __str__(self):
+        return 'Time- {} : {}'.format(self._hour, self._min)
 
 
-time=Time()
+#
+
+
+time = Time(10, 56)
+print(time)
